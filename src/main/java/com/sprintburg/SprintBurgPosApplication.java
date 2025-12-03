ed942a430a43361f8955a3db71d3fa6ee9c46f34
Omar <113750821+GlizSun@users.noreply.github.com>
2025-12-02 23:09:52 -0600

    Commit Inicial

---
+++ src/main/java/com/sprintburg/SprintBurgPosApplication.java
@@ -0,0 +1,13 @@
+package com.sprintburg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBurgPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBurgPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprintburg/SprintBurgPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintburg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBurgPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
